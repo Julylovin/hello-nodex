dec7a84b66f9c8f7341dd7e9c08ede05a4362728
TangKang <julylovin@163.com>
2025-03-21 16:34:24 +0800

    --

--- src/esm/index.mjs
+++ src/esm/index.mjs
@@ -7,26 +7,29 @@
 
 // src/esm/index.mjs
 // 注意这里使用了另外一种方式，将所有的命名导出都挂在了 `m` 变量上
-import * as m from './module.mjs'
+import * as m from "./module.mjs";
 
-console.log(typeof m)
-console.log(Object.keys(m))
+console.log(typeof m);
+console.log(Object.keys(m));
 
-m.foo()
-console.log(m.bar)
+m.foo();
+console.log(m.bar);
 
 // src/esm/index.mjs
 import {
-    foo as foo2,  // 这里进行了重命名
-    bar
-  } from './module.mjs'
-  
-  // 就不会造成变量冲突
-  const foo = 1
-  console.log(foo)
-  
-  // 用新的命名来调用模块里的方法
-  foo2()
-  
-  // 这个不冲突就可以不必处理
-  console.log(bar)
+  foo as foo2, // 这里进行了重命名
+  bar,
+} from "./module.mjs";
+
+// 就不会造成变量冲突
+const foo = 1;
+console.log(foo);
+
+// 用新的命名来调用模块里的方法
+foo2();
+
+// 这个不冲突就可以不必处理
+console.log(bar);
+
+import md5 from "md5";
+console.log(md5("Hello World"));
