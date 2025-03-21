77478d5cb1ee129281006917a576b3b932c7cd1c
TangKang <julylovin@163.com>
2025-03-21 16:10:51 +0800

    --

--- src/esm/index.mjs
+++ src/esm/index.mjs
@@ -1,3 +1,16 @@
 // src/esm/index.mjs
-import m from './module.mjs'
-console.log(m)
+// import m from './module.mjs'
+// console.log(m)
+// import { foo as foox, bar } from './module.mjs'
+// foox()
+// console.log(bar)
+
+// src/esm/index.mjs
+// 注意这里使用了另外一种方式，将所有的命名导出都挂在了 `m` 变量上
+import * as m from './module.mjs'
+
+console.log(typeof m)
+console.log(Object.keys(m))
+
+m.foo()
+console.log(m.bar)
--- src/esm/module.mjs
+++ src/esm/module.mjs
@@ -1,2 +1,7 @@
+  // src/esm/module.mjs
+// export default 'Hello World'
 // src/esm/module.mjs
-export default 'Hello World'
+export function foo() {
+  console.log("Hello World");
+}
+export const bar = "Hello World from bar.";
