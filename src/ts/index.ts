76fc189ec29c5d0d6b1e1f036cfc00fa0fd3ee77
TangKang <julylovin@163.com>
2025-04-01 16:07:55 +0800

    --

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -15,3 +15,37 @@ class User extends UserBase {
 // 这个变量拥有上面两个类的所有属性和方法
 const petter: User = new User('Petter')
 petter.getName()
+
+
+// 可以在 demo 里运行这段代码
+function counter(count: number | string) {
+  console.log(`The current count is: ${count}.`)
+}
+
+// 不论传数值还是字符串，都可以达到的目的
+counter(1)  // The current count is: 1.
+counter('2')  // The current count is: 2.
+
+
+// 只有返回 null 值才能定义返回类型为 null
+function sayHi(name: string): void {
+  // 这里判断参数不符合要求则提前终止运行，但它没有返回值
+  if (!name) return
+
+  // 否则正常运行
+  console.log(`Hi, ${name}!`)
+}
+
+
+sayHi('11') // Hi, Petter!
+
+// 注意这里的返回值类型
+function queryData(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello World')
+    }, 3000)
+  })
+}
+
+queryData().then((data) => console.log(data))
